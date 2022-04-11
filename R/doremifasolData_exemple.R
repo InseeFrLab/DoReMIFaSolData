90937b665a0b9c58e3597b8660bbd507129ee14a
ddotta <damien.dotta@live.fr>
2022-04-11 16:37:07 +0200

    Ajout fonction doremifasolData_exemple()

---
+++ R/doremifasolData_exemple.R
@@ -0,0 +1,19 @@
+#' Récupérer le chemin vers les fichiers exemples de DoremifasolData
+#'
+#' {DoremifasolData} est livré avec un certain nombre de fichiers 
+#' (plats ou issus de tableurs) dans le répertoire `inst/extdata`
+#' Cette fonction en facilite leur accès en retournant leurs chemins.
+#'
+#' @param fichier Nom du fichier. Si `NULL`, les fichiers exemples sont listés.
+#' @export
+#' @examples
+#' doremifasolData_exemple()
+#' doremifasolData_exemple("communes-01012019.csv")
+
+doremifasolData_exemple <- function(fichier = NULL) {
+  if (is.null(fichier)) {
+    dir(system.file("extdata", package = "doremifasolData"))
+  } else {
+    system.file("extdata", fichier, package = "doremifasolData", mustWork = TRUE)
+  }
+}
